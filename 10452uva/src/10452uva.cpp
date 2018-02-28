0d09f9b6f97800966a297106de9bf3d547d95778
Nahla <nahlaglal@gmail.com>
2018-02-28 15:03:31 +0100

    Marcus

--- 10452uva/src/10452uva.cpp
+++ 10452uva/src/10452uva.cpp
@@ -1,15 +1,64 @@
-//============================================================================
-// Name        : 10452uva.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+#include <bits/stdc++.h>
 
-#include <iostream>
-using namespace std;
+using namespace std ;
+
+vector<string> dirc;
+int a,b,index,curx,cury ;
+char arr[9][9] ;
+string s="@IEHOVA#" ;
+
+bool valid(int x, int y){
+	if(x<0 || x==a) return false ;
+	else if(y<0 || y==b) return false ;
+	else return true ;
+}
+
+void path(int x,int y){
+	if(!valid(x,y)) return ;
+	if(arr[x][y]==s[7] && index==7){
+		if(x+1==curx) dirc.push_back("forth") ;
+		else if(y-1==cury) dirc.push_back("right") ;
+		else if(y+1==cury) dirc.push_back("left") ;
+		return ;
+	}
+	if(arr[x][y]==s[index]){
+		index++ ;
+		if(x+1==curx) dirc.push_back("forth") ;
+		else if(y-1==cury) dirc.push_back("right") ;
+		else if(y+1==cury) dirc.push_back("left") ;
+		curx=x ;
+		cury=y ;
+		path(x-1,y) ;
+		path(x,y-1) ;
+		path(x,y+1) ;
+	}
+}
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	//	freopen("input.txt","r",stdin);
+	//	freopen("output.txt","w",stdout);
+	int n;
+	cin>>n ;
+	while(n--){
+		dirc.clear() ;
+		cin>>a>>b ;
+		for(int i=0 ; i<a ; i++){
+			for(int j=0 ; j<b ; j++){
+				cin>>arr[i][j] ;
+				if(arr[i][j]=='@'){
+					curx=i ;
+					cury=j ;
+				}
+			}
+		}
+		index=0 ;
+		path(curx,cury) ;
+		for(int i=0 ; i<dirc.size() ; i++){
+			if(i) cout<<" "<<dirc[i] ;
+			else cout<<dirc[i] ;
+		}
+		cout<<endl ;
+	}
+	return 0 ;
 }
